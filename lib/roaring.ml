3a592c19447810206b69a517dec95e8f51a930c7
Travis Brady <travis.brady@gmail.com>
2016-01-07 16:16:52 -0600

    checkpoint

--- lib/roaring.ml
+++ lib/roaring.ml
@@ -1,33 +1,44 @@
 open Ctypes
-
-let x = 0
+let printf = Printf.printf
 
 module C = Roaring_bindings.C(Roaring_generated)
 
-type t = C.bitset_container_s structure
+type t = C.bitset_container_t structure
+
+(*
+let create () = 
+    C.bitset_container_create
+*)
+
+let _create () =
+    let ret = allocate_n 
+        ~finalise: (fun x -> printf "hi\n")
+        C.bitset_container_t ~count:1 in
+    ret
 
 let create = C.bitset_container_create
+
 let set bs i = C.bitset_container_set bs (Unsigned.UInt16.of_int i)
 let unset bs i = C.bitset_container_unset bs (Unsigned.UInt16.of_int i)
 let get bs i = C.bitset_container_get bs (Unsigned.UInt16.of_int i)
 
 let bitset_container_or bs_a bs_b =
-    let out = make C.bitset_container_s in
-    let res = C.bitset_container_or bs_a bs_b bs_b in
+    let out = create () in
+    let res = C.bitset_container_or bs_a bs_b out in
     out
 
 let bitset_container_or_nocard bs_a bs_b =
-    let out = make C.bitset_container_s in
+    let out = create () in
     let res = C.bitset_container_or bs_a bs_b bs_b in
     out
 
 let bitset_container_and bs_a bs_b =
-    let out = make C.bitset_container_s in
+    let out = create () in
     let res = C.bitset_container_and bs_a bs_b bs_b in
     out
 
 let bitset_container_and_nocard bs_a bs_b =
-    let out = make C.bitset_container_s in
+    let out = create () in
     let res = C.bitset_container_and bs_a bs_b bs_b in
     out
 
